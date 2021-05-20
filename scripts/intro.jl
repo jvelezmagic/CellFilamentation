74fd9db6992bd2e52d5721f41772143eba39e06b
DrWatson <no@mail>
2021-05-19 21:28:38 -0500

    Enhance example plot.

--- scripts/intro.jl
+++ scripts/intro.jl
@@ -4,5 +4,7 @@ using DrWatson
 cell = Cell(max_height = 4.0);
 t_ramp, toxin_ramp = toxin_entry(10, 100, 100, 5, false);
 
-simulation, out_cell = simulate_filamentation(cell, t_ramp, toxin_ramp, trajectories = 100)
-plot(simulation)
+sim, out_cell = simulate_filamentation(cell, t_ramp, toxin_ramp, trajectories = 1000)
+simm = EnsembleSummary(sim)
+
+plot(simm, fillalpha = 0.2, fillcolor = "gray", linecolor = "black", layout = (2, 1))
