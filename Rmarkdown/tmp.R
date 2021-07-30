694a3a585ca6459cd9656805768999e214b69301
Jesús Vélez Santiago <jvelezmagic@gmail.com>
2021-07-29 22:20:04 -0500

    First draft of modeling

--- Rmarkdown/tmp.R
+++ Rmarkdown/tmp.R
@@ -1,19 +1,19 @@
 ---
-title: "Experiment Data Exploration"
+  title: "Experiment Data Exploration"
 author: "Jesús Vélez Santiago"
 date: "`r format(Sys.Date(), '%Y-%m')`"
 output: 
   html_document:
-    theme: readable
-    highlight: kate
-    toc: true
-    toc_float: true
-    toc_depth: 3
-    code_folding: show
-    self_contained: true
+  theme: readable
+highlight: kate
+toc: true
+toc_float: true
+toc_depth: 3
+code_folding: show
+self_contained: true
 ---
-
-```{r setup, include=FALSE}
+  
+  ```{r setup, include=FALSE}
 knitr::opts_chunk$set(
   echo = TRUE,
   message = FALSE,
@@ -48,7 +48,7 @@ processed_lineages_df <- lineages_df %>%
   mutate(
     gfp = log10(gfp),
     ds_red = log10(ds_red),
-    across(contains("filamentaded_"), ~factor(.x, levels = c(FALSE, TRUE), labels = c("Not filamentaded", "Filamentaded")))
+    across(contains("filamentaded_"),~factor(.x, c(FALSE, TRUE), c("Not filamentaded", "Filamentaded")))
   ) %>% 
   add_count(experiment_id, trap_id, track_id, time) %>% 
   filter(n == 1) %>% 
@@ -62,7 +62,7 @@ processed_lineages_df <- lineages_df %>%
 ### Set default plot style
 
 ```{r default_plot_theme}
-theme_set(theme_bw() + theme(legend.position = "top"))
+theme_set(theme_bw())
 ```
 
 
@@ -86,7 +86,7 @@ donout_df <- processed_lineages_df %>%
 donout_total <- donout_df %>% 
   pull(n) %>% 
   sum()
-  
+
 donout_df %>% 
   ggplot(
     aes(
@@ -102,7 +102,7 @@ donout_df %>%
     aes(
       fill=filamentaded_track,
       group=experiment_id
-      )
+    )
   ) +
   geom_label(x = 2, aes(y = label_position, label = label), size=3.5) +
   coord_polar(theta = "y") +
@@ -340,7 +340,7 @@ survival_probability_df <- processed_lineages_df %>%
     tibble(
       plot = list(
         ggplot(data = .x, aes(x = initial_gfp)) +
-        geom_histogram(breaks = gfp_breaks)
+          geom_histogram(breaks = gfp_breaks)
       )
     )
   }) %>% 
@@ -405,19 +405,28 @@ status_points_df <- processed_lineages_df %>%
     initial_gfp = first(gfp),
     sos_gfp = gfp[first_true],
     end_gfp = last(gfp),
+    #diff_sos_initial_gfp = sos_gfp -initial_gfp,
+    #diff_end_sos_gfp = end_gfp - sos_gfp,
+    diff_end_intial_gfp = end_gfp - initial_gfp,
     initial_length = first(length),
     sos_length = length[first_true],
     end_length = last(length),
+    #diff_sos_initial_length = sos_length - initial_length,
+    #diff_end_sos_length = end_length - sos_length,
+    diff_end_intial_length = end_length - initial_length,
     initial_time = first(centered_frame) * 10,
     sos_time = centered_frame[first_true] * 10,
     end_time = last(centered_frame) * 10,
     life_time = end_time - initial_time,
-    is_long_track = first(centered_frame) < unique(centered_antibiotic_start_frame) &&
-      last(centered_frame) > unique(centered_antibiotic_end_frame),
-    is_long_track = "Looon",
+    #diff_sos_intial_time = sos_time - initial_time,
+    #diff_end_sos_time = end_time - sos_time,
+    #diff_end_intial_time = end_time - initial_time,
+    is_survivor = initial_time < unique(centered_antibiotic_end_frame) * 10 &&
+      end_time > unique(centered_antibiotic_end_frame) * 10,
+    is_survivor = ifelse(is_survivor, "Survived", "Dit not survive"),
+    is_survivor = factor(is_survivor),
     .groups = "drop"
   ) %>% 
-  #filter(is_long_track) %>% 
   #filter(initial_frame <= sos_frame, sos_frame <= end_frame) %>% 
   glimpse()  
 ```
@@ -444,7 +453,7 @@ status_points_df %>%
 
 ```{r}
 status_points_df %>% 
-  group_by(experiment_id, filamentaded_track, life_time, is_long_track) %>% 
+  group_by(experiment_id, filamentaded_track, life_time) %>% 
   summarize(
     n = n(),
     initial_length = median(initial_length),
@@ -461,7 +470,7 @@ status_points_df %>%
   ) %>% 
   ggplot(aes(x = life_time, y = value)) +
   geom_line(aes(group = life_time)) +
-  geom_point(aes(color = length_type, shape = is_long_track), alpha = 1/1) +
+  geom_point(aes(color = length_type), alpha = 1/1) +
   facet_grid(filamentaded_track ~ experiment_id) +
   theme_bw() +
   theme(
@@ -476,7 +485,7 @@ status_points_df %>%
 
 ```{r}
 status_points_df %>% 
-  group_by(experiment_id, filamentaded_track, life_time, is_long_track) %>% 
+  group_by(experiment_id, filamentaded_track, life_time) %>% 
   summarize(
     n = n(),
     initial_gfp = median(initial_gfp),
@@ -493,7 +502,7 @@ status_points_df %>%
   ) %>% 
   ggplot(aes(x = life_time, y = value)) +
   geom_line(aes(group = life_time)) +
-  geom_point(aes(color = gfp_type, shape = is_long_track), alpha = 1/1) +
+  geom_point(aes(color = gfp_type), alpha = 1/1) +
   facet_grid(filamentaded_track ~ experiment_id) +
   theme_bw() +
   theme(
@@ -505,3 +514,157 @@ status_points_df %>%
     color = "GFP type"
   )
 ```
+
+```{r}
+library(tidymodels)
+
+model_data <- status_points_df %>% 
+  select(is_survivor, filamentaded_track, contains("gfp"), contains("length"), -contains("diff"), -contains("sos")) %>% 
+  mutate(
+    out = interaction(is_survivor, filamentaded_track),
+    out = as.character(out),
+    out = as.factor(out)
+  ) %>% 
+  select(-is_survivor, -filamentaded_track) %>% 
+  glimpse()
+
+count(model_data, out)
+```
+
+
+```{r}
+set.seed(123)
+model_data_split <- initial_split(model_data, prop = 0.75, strata = out)
+
+training_data <- training(model_data_split)
+testing_data <- testing(model_data_split)
+
+model_split
+```
+
+```{r}
+data_folds <- vfold_cv(training_data, v = 20, strata = out)
+data_folds
+```
+
+
+```{r}
+library(themis)
+
+data_recipe <- recipe(out ~ ., data = training_data) %>% 
+  #step_corr(all_numeric(), threshold = 0.8) %>%
+  step_normalize(all_numeric(), -contains("time")) %>% 
+  step_zv(all_predictors()) %>% 
+  step_dummy(all_nominal(), -all_outcomes()) %>% 
+  step_downsample(out)
+
+summary(data_recipe)
+```
+
+```{r}
+dt_tune_model <- decision_tree(
+  mode = "classification",
+  engine = "rpart",
+  cost_complexity = tune(),
+  tree_depth = tune(),
+  min_n = tune()
+)
+
+dt_tune_model <- rand_forest(
+  mode = "classification",
+  engine = "ranger",
+  mtry = tune(),
+  trees = tune(),
+  min_n = tune()
+)
+
+dt_tune_model
+```
+
+```{r}
+set.seed(123)
+dt_grid <- grid_random(
+  mtry() %>% range_set(c(2, 3)),
+  trees(),
+  min_n(),
+  size = 10
+)
+
+dt_grid
+```
+
+```{r}
+data_wkfl <- workflow() %>% 
+  add_model(dt_tune_model) %>% 
+  add_recipe(data_recipe)
+
+data_wkfl
+```
+
+```{r}
+dt_tuning <- data_wkfl %>% 
+  tune_grid(
+    resamples = data_folds,
+    grid = dt_grid
+  )
+
+dt_tuning %>% 
+  show_best(metric = "roc_auc", n = 5)
+```
+```{r}
+best_dt_model <- dt_tuning %>% 
+  select_best(metric = "roc_auc")
+
+best_dt_model
+```
+
+```{r}
+final_data_wkfl <- data_wkfl %>% 
+  finalize_workflow(best_dt_model)
+
+final_data_wkfl
+```
+
+```{r}
+data_wf_fit <- final_data_wkfl %>% 
+  fit(data = training_data)
+
+tree_fit <- data_wf_fit %>% 
+  extract_fit_parsnip()
+```
+
+```{r}
+vip::vip(tree_fit)
+```
+
+```{r}
+data_final_fit <- final_data_wkfl %>% 
+  last_fit(split = model_data_split)
+
+data_final_fit %>% 
+  collect_metrics()
+```
+
+
+```{r}
+data_final_fit %>% 
+  collect_predictions() %>% 
+  roc_curve(truth = is_survivor, .estimate = .pred_Survived) %>% 
+  identity() %>% 
+  autoplot()
+```
+
+```{r}
+tree_predictions <- data_final_fit %>% collect_predictions()
+
+conf_mat(tree_predictions, truth = is_survivor, estimate = .pred_class) %>% 
+  autoplot()
+```
+
+
+```{r}
+status_points_df %>% 
+  count(is_survivor) %>% 
+  identity()
+```
+
